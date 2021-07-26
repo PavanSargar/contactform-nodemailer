1f88053d2846ed1ddf56ac500788f4bad6771e0a
Pavan Sargar <pavansargar36@gmail.com>
2021-07-27 00:54:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const user = process.env.USER;
+const pass = process.env.PASS;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/contactform.html")
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user,
+            pass
+        },
+    });
+
+    const mailOptions = {
+        from: req.body.email,
+        to: "pawancontact8@gmail.com",
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, responose) => {
+        if(error) {
+            console.log(error);
+            res.send("error")
+        } else {
+            console.log("Email Sent");
+            res.send("success")
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}...`);
+});
